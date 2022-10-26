a560b1f6a5bb3988141782446cd86e53ce504ca6
NikolasMatos <npequenomatos@gmail.com>
2022-10-26 11:22:16 -0300

    exercicio-resolvido

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -11,16 +11,42 @@ import {
 import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
+  const [lista, setLista] = useState([
+    "estudar",
+    "fazer ecxercício",
+    "descansar",
+  ]); //criei a lista aqui
+
   const [novaTarefa, setNovaTarefa] = useState("");
 
+  const removeTarefa = (tarefa) => {
+    const listaFiltrada = lista.filter((item) =>{
+      return item !== tarefa
+    })
+    setLista(listaFiltrada)
+  }; //criando uma função de remover tarefas.
+
+  const adicionaTarefa = () => {
+    const novaLista = [...lista]
+    novaLista.push(novaTarefa)
+    setLista(novaLista)
+    setNovaTarefa("")
+  }; //criando a função para adicionar a nova tarefa.
+
+  const renderizarLista = lista.map((tarefa, index) => {
+    return (
+      <Tarefa key={index}>
+        <p>{tarefa}</p>
+        <RemoveButton onClick={() => removeTarefa(tarefa)}>
+          <img src={bin} alt="" width="16px" />
+        </RemoveButton>
+      </Tarefa>
+    );
+  }); //criando uma função map aqui.
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
-
-  const adicionaTarefa = () => {};
-
-  const removeTarefa = () => {};
 
   return (
     <ListaTarefasContainer>
@@ -30,18 +56,11 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
-        <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
-        </ul>
+        <ul>{renderizarLista}</ul>
       </ListaContainer>
     </ListaTarefasContainer>
   );
